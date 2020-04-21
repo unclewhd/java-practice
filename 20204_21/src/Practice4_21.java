e02fe78dca02dc52a65c0d5d05f4a16e1b724087
unclewhd <316249593@qq.com>
2020-04-21 23:59:17 +0800

    练习

---
+++ 20204_21/src/Practice4_21.java
@@ -0,0 +1,13 @@
+
+public class Practice4_21 {
+    public int aMethod(){
+        int i = 0;
+        i++;
+        return i;
+
+    }
+    static boolean Paddy;
+        public static void main(String args[]){
+            System.out.println(Paddy);
+        }
+}
