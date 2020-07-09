7298920bb444914a407c248d767d2b235326fd6b
unclewhd <316249593@qq.com>
2020-07-09 22:23:13 +0800

    练习

---
+++ idea练习1.1/2020_7_9/2020_7_9/src/com/wanghaidong/DBUtil.java
@@ -0,0 +1,33 @@
+package com.wanghaidong;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static volatile DataSource dataSource = null;
+    public static Connection getConnection() throws SQLException {
+        if(dataSource == null){
+            synchronized (DBUtil.class){
+                if(dataSource == null){
+                    dataSource = initDataSource();
+                }
+            }
+        }
+
+        return dataSource.getConnection();
+    }
+
+    private static DataSource initDataSource() {
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setServerName("127.0.0.1");
+        mysqlDataSource.setPort(3306);
+        mysqlDataSource.setUser("root");
+        mysqlDataSource.setPassword("");
+        mysqlDataSource.setDatabaseName("");
+
+        return mysqlDataSource;
+    }
+}
